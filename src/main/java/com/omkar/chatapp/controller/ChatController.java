d16770406d1f6fb76d70b65330b98d4cb7f03b5f
ShindeOm91 <oshinde91199@gmail.com>
2026-01-13 21:04:08 +0530

    Changes

--- src/main/java/com/omkar/chatapp/controller/ChatController.java
+++ src/main/java/com/omkar/chatapp/controller/ChatController.java
@@ -12,7 +12,6 @@ import com.omkar.chatapp.model.ChatMessage;
 @Controller
 public class ChatController {
 	
-
     @MessageMapping("/chat.sendMessage")
     @SendTo("/topic/public")
     public ChatMessage sendMessage(@Payload ChatMessage chatMessage) {
